b2aa99bf9222f643bf9eb5c618507b726d82d6d0
chickenpotpie0 <43829709+chickenpotpie0@users.noreply.github.com>
2022-03-02 00:26:30 -0600

    Update PlayerHealthBar.cs
    Attempt at making the health bars move smoothly. the speed the bar is going to move should be 1/x, which might be weird, so gotta see how it looks. Can be editted but could be cool

--- Assets/Scripts/PlayerHealthBar.cs
+++ Assets/Scripts/PlayerHealthBar.cs
@@ -6,11 +6,18 @@ public class PlayerHealthBar : MonoBehaviour
 {
     Vector3 localScale;
     public GameObject parentPlayer;
+    float currentHealth;
+    float targetHealth;
+    float previousTargetHealth;
+    int timer;
 
     // Start is called before the first frame update
     void Start()
     {
         localScale = transform.localScale;
+        currentHealth = parentPlayer.GetComponent<Player>().health;
+        targetHealth = currentHealth;
+        timer = 0;
     }
 
     // Update is called once per frame
@@ -22,9 +29,24 @@ public class PlayerHealthBar : MonoBehaviour
         }
         else
         {
+            if(targetHealth != parentPlayer.GetComponent<Player>().health)
+            {
+                targetHealth = parentPlayer.GetComponent<Player>().health;
+                timer = (int)(MathF.Abs(currentHeath - targetHealth) / 5);
+            }
+            if(timer > 0)
+            {
+                currentHealth -= (currentHealth - targetHealth) / timer;
+                timer--;
+            }
+            localScale.x = (currentHealth / 200f);
+            transform.localScale = localScale;
+            transform.position = new Vector3(parentPlayer.transform.position.x, (parentPlayer.transform.position.y - 0.5f), parentPlayer.transform.position.z);
+            /* Chris' previous code that I am rewriting to add smooth health transitions
             localScale.x = (parentPlayer.GetComponent<Player>().health / 200f);
             transform.localScale = localScale;
             transform.position = new Vector3(parentPlayer.transform.position.x, (parentPlayer.transform.position.y - 0.5f), parentPlayer.transform.position.z);
+            */
         }
     }
     
